0e7dba665f3200ac5aaf031ab0e8c981665471af
Arman Altafi <84268732+Arman-ATI@users.noreply.github.com>
2025-04-28 17:54:49 +0330

    Fix the NowBar doesn't update after the media session has ended
    Scenario:
    When you close the Telegram audio player with "x" without pausing music, then it will be stuck in the NowBar until you press on it or play a different player.

--- packages/SystemUI/src/com/android/keyguard/NowBarHolder.kt
+++ packages/SystemUI/src/com/android/keyguard/NowBarHolder.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2023-2024 The risingOS Android Project
+ * Copyright (C) 2025 The RisingOS Revived Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +21,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
@@ -37,6 +40,9 @@ class NowBarHolder @JvmOverloads constructor(
     private var mMediaSessionManagerHelper: MediaSessionManagerHelper
     
     private var isChargingStatusHandled = false
+    private var wasPlayingBefore = false
+    private var mediaCheckHandler = Handler(Looper.getMainLooper())
+    private var mediaCheckRunnable: Runnable? = null
 
     private val batteryReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -79,14 +85,50 @@ class NowBarHolder @JvmOverloads constructor(
         mController.removeNowBarHolder(this)
         context.unregisterReceiver(batteryReceiver)
         mMediaSessionManagerHelper.removeMediaMetadataListener(this)
+        stopMediaCheckTask()
     }
 
     override fun onMediaMetadataChanged() {
-        showMusicNowBarIfNeeded()
+        handleMediaStateChange()
     }
 
     override fun onPlaybackStateChanged() {
-        showMusicNowBarIfNeeded()
+        handleMediaStateChange()
+    }
+
+    private fun handleMediaStateChange() {
+        val isPlaying = mMediaSessionManagerHelper.isMediaPlaying()
+        
+        if (isPlaying) {
+            wasPlayingBefore = true
+            mViewPager?.setCurrentItem(0)
+            
+            stopMediaCheckTask()
+        } else if (wasPlayingBefore) {
+            wasPlayingBefore = false
+            startMediaCheckTask()
+        }
+    }
+    
+    private fun startMediaCheckTask() {
+        stopMediaCheckTask()
+        
+        mediaCheckRunnable = Runnable {
+            if (!mMediaSessionManagerHelper.isMediaPlaying()) {
+                if (isChargingStatusHandled) {
+                    mViewPager?.setCurrentItem(1)
+                }
+            }
+        }
+        
+        mediaCheckHandler.postDelayed(mediaCheckRunnable, 2000)
+    }
+    
+    private fun stopMediaCheckTask() {
+        mediaCheckRunnable?.let {
+            mediaCheckHandler.removeCallbacks(it)
+            mediaCheckRunnable = null
+        }
     }
 
     private inner class NowBarAdapter(private val context: Context) : PagerAdapter() {
@@ -154,10 +196,5 @@ class NowBarHolder @JvmOverloads constructor(
         return chargePlug == BatteryManager.BATTERY_PLUGGED_AC
                 || chargePlug == BatteryManager.BATTERY_PLUGGED_USB
                 || chargePlug == BatteryManager.BATTERY_PLUGGED_WIRELESS
-    }
-
-    private fun showMusicNowBarIfNeeded() {
-        if (!mMediaSessionManagerHelper.isMediaPlaying()) return
-        mViewPager?.setCurrentItem(0)
     }
 }
